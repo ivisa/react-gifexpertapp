8d6806e42656ded683da7417d6c43748a49e8ee6
ivanigor <izapata.simbala@gmail.com>
2022-04-23 12:28:41 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=U4y5XeDLL6bCu4PXmQZCU9EIfhrxUISd`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifs;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { GifExpertApp } from './GifExpertApp';
 import reportWebVitals from './reportWebVitals';
+import 'animate.css'
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GifExpertApp/>
   </React.StrictMode>
 );
 
